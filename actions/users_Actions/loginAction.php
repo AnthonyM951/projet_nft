0d34efb901bdc98a95001d57802acc42daacb3c2
mohamedamine-tarhouni <80169795+mohamedamine-tarhouni@users.noreply.github.com>
2022-02-11 00:38:54 +0100

    Add files via upload

---
+++ actions/users_Actions/loginAction.php
@@ -0,0 +1,34 @@
+<?php 
+require ('../actions/database.php');
+if (isset($_POST['submit'])){
+    if (!empty($_POST['username']) and !empty($_POST['password'])) {
+        $userdata = htmlspecialchars($_POST['username']); //Email or Username
+        $password = htmlspecialchars($_POST['password']); //password
+
+        //verify if user exists
+        $req_User_Exists = $mysql->prepare("SELECT * FROM users WHERE Mail= ? OR Username= ? ");
+        $req_User_Exists->execute(array($userdata, $userdata));
+        //if user exists we verify if the password is correct
+        if ($req_User_Exists->rowCount() > 0) {
+            $userInfos = $req_User_Exists->fetch();
+            if (password_verify($password, $userInfos['Passwd'])) {
+
+                // //user authentification
+                // $_SESSION['auth'] = true;
+                // $_SESSION['ID_User'] = $userInfos['ID_User'];
+                // $_SESSION['Username'] = $userInfos['Username'];
+                // $_SESSION['MDP'] = $userInfos['MDP'];
+                // $_SESSION['Mail'] = $userInfos['Mail'];
+                header('Location: index.php');
+            } else { //faux mot de passe
+                $error_Msg = "verify your information";
+            }
+        } else { //Utilisateur inexistant
+            $error_Msg = "verify your information";
+        }
+    }else{
+        $userdata = htmlspecialchars($_POST['username']); //Email or Username
+        $password = htmlspecialchars($_POST['password']); 
+        $error_Msg= "Please fill the form";
+    }
+}
---
+++ views/index.php
@@ -0,0 +1,338 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>evaluation_ydays</title>
+    <link rel="stylesheet" href="../assets/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;500&family=Roboto:wght@100&display=swap" rel="stylesheet">
+
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;500&family=Roboto:wght@100;500&display=swap"
+        rel="stylesheet"> -->
+</head>
+
+<body>
+    <header class="navbar">
+        <div class="navbar__container">
+            <div class="navbar__logo">
+                <a href="index.php"><img src="../assets/images/logo.PNG" alt="logo_site"></a>
+            </div>
+            <div class="navbar__content">
+                <nav>
+                    <ul>
+                        <li>GET STARTED</li>
+                        <li>REALISATION</li>
+                        <li>WHY GYNFT?</li>
+                        <li>FAQ</li>
+                        <li>CONTACT</li>
+
+                    </ul>
+                </nav>
+            </div>
+            <div class="navbar_connect">
+                <!-- <button class="connect">LOGIN</button> -->
+                <a href="login.php"> <input id="connect" value="LOGIN"></a>
+                <a href="signUp.php"> <input id="connect" value="REGISTER"></a>
+                <!-- <button class="connect">REGISTER</button> -->
+            </div>
+            <!-- <div class="navbar__icons">
+                <div class="navbar__favorite">
+                    <svg width="24px" height="24px" fill="#111" viewBox="0 0 24 24">
+                        <path
+                            d="M21.11 4a6.6 6.6 0 0 0-4.79-1.92A6.27 6.27 0 0 0 12 3.84 6.57 6.57 0 0 0 2.89 4c-2.8 2.68-2.45 7.3.88 10.76l6.84 6.63A2 2 0 0 0 12 22a2 2 0 0 0 1.37-.54l.2-.19.61-.57c.6-.57 1.42-1.37 2.49-2.41l2.44-2.39 1.09-1.07c3.38-3.55 3.8-8.1.91-10.83zm-2.35 9.4l-.25.24-.8.79-2.44 2.39c-1 1-1.84 1.79-2.44 2.36L12 20l-6.83-6.68c-2.56-2.66-2.86-6-.88-7.92a4.52 4.52 0 0 1 6.4 0l.09.08a2.12 2.12 0 0 1 .32.3l.9.94.9-.94.28-.27.11-.09a4.52 4.52 0 0 1 6.43 0c1.97 1.9 1.67 5.25-.96 7.98z">
+                        </path>
+                    </svg>
+                </div>
+                <div class="navbar__cart">
+                    <svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="24px" height="24px"
+                        viewBox="0 0 512.000000 512.000000" preserveAspectRatio="xMidYMid meet">
+
+                        <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)" fill="#000000"
+                            stroke="none">
+                            <path d="M157 4915 l-46 -93 492 -237 c271 -130 493 -238 495 -239 1 -2 112
+-529 247 -1172 134 -643 247 -1179 250 -1191 l5 -23 1444 0 1443 0 51 -40 c88
+-71 153 -186 151 -270 -1 -78 -62 -167 -147 -217 l-47 -28 -1482 -3 -1483 -2
+0 -105 0 -105 1468 0 c1610 0 1517 -3 1648 60 150 73 264 245 264 399 0 126
+-74 273 -199 396 -33 33 -61 67 -61 74 0 13 342 1689 357 1749 l5 22 -1801 0
+c-991 0 -1801 3 -1801 8 0 22 -122 578 -128 583 -7 7 -937 463 -1028 504 l-50
+23 -47 -93z m4598 -1257 c-9 -41 -305 -1476 -305 -1482 0 -3 -602 -5 -1337 -4
+l-1337 3 -158 740 c-87 407 -158 746 -158 753 0 9 337 12 1650 12 l1650 0 -5
+-22z" />
+                            <path d="M2040 889 c-114 -22 -222 -108 -274 -219 -28 -59 -31 -74 -31 -165 0
+-91 3 -106 31 -165 166 -350 678 -276 745 108 36 201 -110 402 -321 442 -60
+11 -87 11 -150 -1z m188 -231 c117 -78 104 -257 -23 -318 -126 -61 -266 26
+-266 164 0 79 32 131 106 171 45 24 133 16 183 -17z" />
+                            <path d="M4172 889 c-160 -31 -289 -166 -312 -327 -17 -123 23 -246 108 -331
+192 -192 513 -142 639 99 26 51 28 64 28 165 0 102 -2 115 -31 175 -54 114
+-160 197 -283 220 -61 11 -88 11 -149 -1z m187 -232 c55 -36 85 -103 76 -165
+-15 -101 -76 -162 -170 -170 -141 -12 -243 124 -187 250 37 85 95 121 185 114
+40 -3 70 -12 96 -29z" />
+                        </g>
+                    </svg>
+                </div>
+            </div> -->
+        </div>
+    </header>
+    <main>
+        <div class="hero">
+            <div class="hero__image">
+                <img src="../assets/images/hero.png" alt="" class="hero__img">
+            </div>
+            <div class="hero__text">
+                <p>GET YOUR OWN NFTs AND BECOME A CRYPTO MILLIONNAIRE</p>
+                <div class="hero__subtext">
+                    <p>With n°1 customised NFT designer in the world</p>
+                </div>
+                <div class="hero_btn">
+                    <button class="btn"><a href="#">BUILD YOUR COLLECTION</a> </button>
+                    <div class="dropMenu">
+                        <button class="btn">
+                            >>
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+        <div class="started_container">
+            <!-- <h1 class="started_title"> LET'S GET STARTED</h1> -->
+            <h1 class="started_title">WHAT'S A NFT?</h1>
+            <p class="started_text">
+                Non-fungible tokens or NFTs are cryptographic assets on a blockchain with unique identification codes
+                and metadata that distinguish them from each other. Unlike cryptocurrencies, they cannot be traded or
+                exchanged at equivalency. This differs from fungible tokens like cryptocurrencies, which are identical
+                to each other and, therefore, can be used as a medium for commercial transactions.
+            </p>
+            <h1 class="started_title">WHY WOULD YOU GET A NFT COLLECTION?</h1>
+            <div class="started_collection">
+                <div class="started_collection_img">
+                    <img src="../assets/images/bored-ape.jpg" alt="ape_nft">
+                </div>
+                <div class="started_collection_text">
+                    <p class="started_text">NFT market is developing very fast, now and there are tones of new arts
+                        created
+                        every day. To get noticed and earn on NFT you must have unique and custom art done by
+                        professionalist. You surely have heard about those NFTs that made people rich. You can be one of
+                        them,
+                        it's not too late.</p>
+                </div>
+            </div>
+        </div>
+        <div class="realisation_container">
+            <div class="realisation_title">
+                <h1>COLLECTION</h1>
+            </div>
+            <div class="realisation_display">
+                <div class="realisation_img">
+
+
+
+
+                    <div class="contener_slide">
+                        <ul class="img_content">
+                            <li><img class="real_img fade" src="../assets/images/bored-ape.jpg" alt=""></li>
+                            <li> <img class="real_img fade" src="../assets/images/bored-ape_copy.jpg" alt=""></li>
+                            <li><img class="real_img fade" src="../assets/images/hero.PNG" alt=""></li>
+                        </ul>
+
+                        <!-- <img class="real_img" src="../assets/images/bored-ape_copy.jpg" alt="">
+
+                        <img class="real_img" src="../assets/images/bored-ape copy_2.jpg" alt=""> -->
+
+                    </div>
+                    <a class="prev" onclick="plusSlides(-1)"> <i id="icon_left" class="fas fa-chevron-circle-left"></i></a>
+                    <a class="next" onclick="plusSlides(1)"><i id="icon_right" class="fas fa-chevron-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="why_container">
+            <div class="why_title">
+                <h1>WHY GYNFT?</h1>
+                <h2>GYNFT propose 3 offers starting at 1950$</h2>
+                <div class="why_text">
+                    <div class="grid_prop">
+                        <div class="prop_card">
+                            <div class="prop_basic">
+                                <h3>BASIC</h3>
+                            </div>
+
+                            <ul class="fa-ul">
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>1 Base character</li>
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>Generate 1000 NFT
+                                    collection
+                                </li>
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited revisions until you
+                                    get satisfied.</li>
+                                <!-- <li>Metadata, json files, rarities in %% provided by you.</li> -->
+                            </ul>
+                            <div class="price_container">
+                                <div class="price">
+                                    <p>___________________</p>
+
+                                    <h1 class="prop_price">$1950</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="prop_card">
+                            <div class="prop_standard">
+                                <h3>STANDARD</h3>
+                            </div>
+
+                            <ul class="fa-ul">
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>Really Good Quality character
+                                </li>
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>30 Accessories</li>
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>15 color backgrounds</li>
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>Generate 1000 NFT collection
+                                </li>
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>Metadata file</li>
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>Free Unlimited revisions
+                                </li>
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>Free all source files</li>
+                            </ul>
+                            <div class="price_container">
+                                <div class="price">
+                                    <p>___________________</p>
+                                    <h1 class="prop_price">$3200</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="prop_card">
+                            <div class="prop_premium">
+                                <h3>PREMIUM</h3>
+                            </div>
+
+                            <ul class="fa-ul">
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>Very high quality Base
+                                    character</li>
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>50 Accessories</li>
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>Generate 10,000 NFT
+                                    collection</li>
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>20 background colors</li>
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>Metadata file</li>
+                                <li><span class="fa-li"><i class="fas fa-check"></i></span>Free all source files</li>
+                            </ul>
+                            <div class="price_container">
+                                <div class="price">
+                                    <p>___________________</p>
+                                    <h1 class="prop_price">$7500</h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- <ul>
+                        <li>Unlimited revisions until you get satisfied.</li>
+                        <li>Professional designers with more than 10 years of experience
+                            in
+                            the business.</li>
+                        <li>Custom hand drawn base design and attributes; generation of 100 NFTs (out of 20 traits)
+                            1000
+                            NFTS (out of 32 different traits) or 10 000+ NFTS (out of 100 different traits) for your
+                            OpenSea, Solana or any other platforms.</li>
+                        <li>Experience, as
+                            we
+                            worked with many NFT creators. We can assure you
+                            satisfaction and all you can expect is high quality custom art done with care.</li>
+                        <li>Metadata, json files, rarities in %% provided by you.</li>
+                        <li>Output I provide is ready to upload on all the NFT marketplaces.</li>
+                    </ul>
+                    <p class="started_text"> We design Custom hand drawn base design and attributes. We know it’s
+                        very
+                        important that your clients need to love those to make them sell well. This offer can be
+                        tailored
+                        for your needs in terms of deadline, price and art style. That’s why I encourage you to
+                        speak
+                        freely
+                        about your expectations. I hope we could do amazing Art collection together!</p> -->
+                </div>
+            </div>
+        </div>
+        <div class="contact">
+            <h1>CONTACT</h1>
+            <div class="contact_container">
+                <div class="contact_text">
+                    <p>If you got any questions or requests before ordering:</p>
+                </div>
+                <div class="contact_name">
+                    <span>NAME:</span>
+                    <input type="text">
+                </div>
+                <div class="contact_email">
+                    <span>EMAIL ADDRESS:</span>
+
+                    <input type="email">
+                </div>
+                <div class="contact_msg">
+                    <span>MESSAGE:</span>
+
+                    <textarea type="text" placeholder="Please describe your problem/ question as accurately as possible. Keep in mind that our general support is not responsible for order problems."></textarea>
+                </div>
+                <button class="contact_submit" type="submit">SUBMIT</button>
+            </div>
+
+
+        </div>
+        </div>
+    </main>
+
+    <footer>
+        <div>
+            <p>
+                LEGAL MENTIONS
+            </p>
+        </div>
+        <div>
+            <p>&copy; GYNFT</p>
+        </div>
+        <div>
+            <p>
+                CONTACT
+            </p>
+        </div>
+
+    </footer>
+</body>
+
+</html>
+<script>
+    var slideIndex = 1;
+    showSlides(slideIndex);
+
+    // Next/previous controls
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    // Thumbnail image controls
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("real_img");
+        var dots = document.getElementsByClassName("dot");
+        if (n > slides.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = slides.length
+        }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
+</script>
