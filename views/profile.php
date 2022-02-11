bda3c1a7136e3ba0c173379ee851d67ce56479ba
AMICHEL2 <anthony.michel@ynov.com>
2022-02-11 02:19:47 +0100

    page profil version une

---
+++ views/profile.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>evaluation_ydays</title>
+    <link rel="stylesheet" href="../assets/profile.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;500&family=Roboto:wght@100&display=swap" rel="stylesheet">
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;500&family=Roboto:wght@100;500&display=swap"
+        rel="stylesheet"> -->
+</head>
+<!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"> -->
+
+<body>
+    <header class="navbar">
+        <div class="navbar__container">
+            <div class="navbar__logo">
+                <a href="index.php"><img src="../assets/images/logo.PNG" alt="logo_site"></a>
+            </div>
+            <!-- <input class="btn" type="submit" value="Create an Account"> -->
+            <div class="navbar_connect">
+                <a href="login.php"> <input id="connect" value="LOGIN"></a>
+                <!-- <button class="connect">REGISTER</button> -->
+            </div>
+
+        </div>
+    </header>
+
+
+    <main>
+        <div class="profile_container">
+            <div class="profile_display">
+                <div class="profile_picture">
+
+                    <img src="../assets/images/profile_pic.jpg" alt="">
+                    <div class="i_cont">
+                        <i class="fa fa-camera"></i>
+                    </div>
+                </div>
+                <div class="profile_name">
+                    <h4>Your name</h4>
+                </div>
+            </div>
+            <div class="profile_desc">
+                <div class="profile_username">
+                    <div class="profile_p">
+                        <p>USERNAME:</p>
+                        <p>Your username</p>
+                    </div>
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </div>
+                <hr>
+                <div class="profile_email">
+                    <div class="profile_p">
+                        <p>EMAIL:</p>
+                        <p>Your email</p>
+                    </div>
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </div>
+                <hr>
+                <div class="profile_password">
+                    <div class="profile_p">
+                        <p>PASSWORD:</p>
+                        <p>***********</p>
+                    </div>
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </div>
+                <hr>
+            </div>
+        </div>
+    </main>
+    <footer>
+        <div>
+            <p>
+                LEGAL MENTIONS
+            </p>
+        </div>
+        <div>
+            <p>&copy; GYNFT</p>
+        </div>
+        <div>
+            <p>
+                CONTACT
+            </p>
+        </div>
+
+    </footer>
+</body>
+
+
+</html>
